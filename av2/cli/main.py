031130bdc91771400a36adf00bbd65262e77bf09
jiancheng <1729303158@qq.com>
2023-02-21 14:24:10 +0800

    style: format code

--- av2/cli/main.py
+++ av2/cli/main.py
@@ -11,18 +11,6 @@ console = Console()
 def av2():
     pass
 
-@av2.command()
-@click.option('-i', '--source_video', help='视频在硬盘中的路径')
-def view(source_video: str = ...):
-    """ view all container """
-    import av2
-    import json
-    import yaml
-
-    with av2.open(source_video) as file:
-        print(file.dict())
-        print(json.dumps(file.dict(),indent=4))
-        print(yaml.dump(file.dict(),indent=4))
 
 @av2.command()
 @click.option('-i', '--source_video', help='视频在硬盘中的路径')
@@ -34,9 +22,8 @@ def info(source_video: str = ...):
 
     with av2.open(source_video) as file:
         print(file.dict())
-        print(json.dumps(file.dict(),indent=4))
-        print(yaml.dump(file.dict(),indent=4))
-        
+        print(json.dumps(file.dict(), indent=4))
+        print(yaml.dump(file.dict(), indent=4))
 
 
 cli = click.CommandCollection(sources=[av2])
